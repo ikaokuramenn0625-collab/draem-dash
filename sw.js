a4fdeed589651f9dc009bcd6eeb851de72ee341f
ryusuke0625 <ryusuke.m.0625@gmail.com>
2026-04-06 18:43:41 +0900

    KPI 3列レイアウト・X/niconico タイル追加・設定モーダル読み取り専用化

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V='v104';
+const V='v108';
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(V).then(c=>c.addAll(['./index.html','./','./manifest.json','./music-hub.html','./music-hub-sync.json'])));
   self.skipWaiting();
